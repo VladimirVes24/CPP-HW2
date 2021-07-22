3778e83d36ac7e3e798f21428f2701019d3123d7
VLADIMIR <veselov2100@mail.ru>
2021-07-23 02:00:10 +0300

    Finished homework

--- CPP HW1.cpp
+++ CPP HW1.cpp
@@ -1,19 +1,99 @@
-// CPP HW1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
-int main()
+/*
+* Задание:
+*
+* 1) Создать и инициализировать переменные пройденных типов данных (short int, int, long long, char, bool, float, double).
+*
+* 2) Создать перечисление (enum) с возможными вариантами символов для игры в крестики-нолики.
+*
+* 3) Создать массив, способный содержать значения такого перечисления и инициализировать его.
+*
+* 4) * Создать структуру (struct) данных «Поле для игры в крестики-нолики» и снабдить его всеми
+* необходимыми свойствами (подумайте что может понадобиться).
+*
+* 5) ** Создать структуру (struct) объединяющую: union (int, float, char) и 3-и битовых поля (флага)
+* указывающими какого типа значение в данный момент содержится в объединении (isInt, isFloat, isChar).
+* Продемонстрировать пример использования в коде этой структуры.
+*/
+
+
+
+//Задание №1
+void Task1()
 {
+	short int firstVar = 32'767;
+	int16_t sameFirstVar = 32'767;
+
+	int secondVar = 2'147'483'647;
+	int32_t sameSecondVar = 2'147'483'647;
+
+	long long int thirdVar = 9'223'372'036'854'775'807;
+	int64_t sameThirdVar = 9'223'372'036'854'775'807;
+
+	char forthVar = 'A';
+
+	bool fifthVar = false;
+
+	float sixthVar = 143.15f;
+
+	double seventhVar = 1567.5646846;
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+//Задание №2
+enum Cell
+{
+	O,
+	X,
+	NONE
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//Задание №3,4
+
+struct ticTacToeGame {
+
+	Cell ticTacToeGameField[3][3] = { { NONE, NONE, NONE },{ NONE, NONE, NONE },{ NONE, NONE, NONE } };
+	std::string playerNames[2] = { "ALEX","JOHN" };
+	int whichPlayerTurn = 0, turnCount = 1;
+	bool gameComplete = false, threeMarkRow = false;
+	int firstPlayerMark = O, secondPlayerMark = X;
+	int Winner;
+
+};
+
+
+//Задание №5
+
+union myVariantType
+{
+	int32_t integer;
+	float decimal;
+	char character;
+};
+
+struct MyVariant
+{
+	myVariantType type;
+	uint16_t isInt : 1;
+	uint16_t isFloat : 1;
+	uint16_t isChar : 1;
+};
+
+void PrintVariantType() 
+{
+	MyVariant newVariant;
+	newVariant.type.decimal = 3.23f;
+	newVariant.isFloat = 1;
+	if (newVariant.isInt == 1) std::cout << "Variant type is Integer!";
+	else if (newVariant.isFloat == 1) std::cout << "Variant type is Float!";
+	else if (newVariant.isChar == 1) std::cout << "Variant type is Char!";
+	else std::cout << "Variant type is not defined.";
+}
+
+int main()
+{
+	PrintVariantType();
+	return 0;
+}
